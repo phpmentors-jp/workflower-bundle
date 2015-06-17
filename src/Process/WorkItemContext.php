b2d7aa29a998544ae6dc1ad23efd800313c7625f
KUBO Atsuhiro <kubo@iteman.jp>
2015-06-17 16:00:47 +0900

    add `WorkItemContext`

---
+++ src/Process/WorkItemContext.php
@@ -0,0 +1,83 @@
+<?php
+/*
+ * Copyright (c) 2015 KUBO Atsuhiro <kubo@iteman.jp>,
+ * All rights reserved.
+ *
+ * This file is part of PHPMentorsWorkflowerBundle.
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the BSD 2-Clause License which accompanies this
+ * distribution, and is available at http://opensource.org/licenses/BSD-2-Clause
+ */
+
+namespace PHPMentors\WorkflowerBundle\Process;
+
+use PHPMentors\Workflower\Process\ProcessContextInterface;
+use PHPMentors\Workflower\Process\WorkItemContextInterface;
+use PHPMentors\Workflower\Workflow\Participant\ParticipantInterface;
+
+class WorkItemContext implements WorkItemContextInterface
+{
+    /**
+     * @var int|string
+     */
+    private $activityId;
+
+    /**
+     * @var ParticipantInterface
+     */
+    private $participant;
+
+    /**
+     * @var ProcessContextInterface
+     */
+    private $processContext;
+
+    /**
+     * @param ParticipantInterface $participant
+     */
+    public function __construct(ParticipantInterface $participant)
+    {
+        $this->participant = $participant;
+    }
+
+    /**
+     * @param int|string $activityId
+     */
+    public function setActivityId($activityId)
+    {
+        $this->activityId = $activityId;
+    }
+
+    /**
+     * @param ProcessContextInterface $processContext
+     */
+    public function setProcessContext(ProcessContextInterface $processContext)
+    {
+        $this->processContext = $processContext;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getActivityId()
+    {
+        return $this->activityId;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getParticipant()
+    {
+        return $this->participant;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getProcessContext()
+    {
+        return $this->processContext;
+    }
+}
